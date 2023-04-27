5c931e69d523e3547d547926ea219ae8ca33099d
Nesa Nosrattalab <116934656+NesaNst@users.noreply.github.com>
2023-04-26 23:07:56 -0700

    Array.prototype.last (java)

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {555,5,5,5,7};
+        System.out.println(returnLast(arr));
+    }
+
+    public static int returnLast(int[] arr) {
+       if (arr.length == 0) {
+           return -1;
+       } else {
+           if (arr.length <= 1000) {
+               return arr[arr.length-1];
+           } else {
+               return arr[1000];
+           }
+       }
+    }
+}
